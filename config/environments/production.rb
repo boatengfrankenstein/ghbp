a0def97f18573382a039e4e3211307a8b0b8acd9
daniel boakye <boatengfrankenstein@gmail.com>
2018-04-13 23:26:36 -0400

    Open u

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,9 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
+
+  config.log_level = :debug # In any environment initializer, or
+  Rails.logger.level = 0 # at any time
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
